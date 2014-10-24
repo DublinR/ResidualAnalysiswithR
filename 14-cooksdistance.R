30efe214762c33c296c3dcd8e3f72952c55f10ce
Dublin R <DublinRUserGroup@gmail.com>
2014-10-24 13:41:35 +0100

    Update 14-cooksdistance.R

--- 14-cooksdistance.R
+++
@@ -1,5 +0,0 @@
-In statistics, Cook's distance or Cook's D is a commonly used estimate of the influence of a data point when performing least squares regression analysis.[1] In a practical ordinary least squares analysis, Cook's distance can be used in several ways: to indicate data points that are particularly worth checking for validity; to indicate regions of the design space where it would be good to be able to obtain more data points. It is named after the American statistician R. Dennis Cook, who introduced the concept in 1977.
- 
- 
-Cook's distance measures the effect of deleting a given observation. Data points with large residuals (outliers) and/or high leverage may distort the outcome and accuracy of a regression. Points with a large Cook's distance are considered to merit closer examination in the analysis. It is calculated as:
- D i =∑ n j=1 (Y ^  j  −Y ^  j(i) ) 2  p MSE  , 
